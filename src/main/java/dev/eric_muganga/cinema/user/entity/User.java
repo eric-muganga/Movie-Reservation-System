3275f2b194c2ac3377e24e58612d1fd1cb69c6b9
eric-muganga <105128548+eric-muganga@users.noreply.github.com>
2026-06-04 13:31:42 +0200

    feat(user): add User and Role JPA entities

---
+++ src/main/java/dev/eric_muganga/cinema/user/entity/User.java
@@ -0,0 +1,46 @@
+package dev.eric_muganga.cinema.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "authO_sub", unique = true, nullable = false)
+    private String authOSub;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+}
+
