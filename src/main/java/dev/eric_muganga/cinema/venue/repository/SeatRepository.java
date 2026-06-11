3f135877ec3314e5a0896ea8e8073a8243410551
eric-muganga <105128548+eric-muganga@users.noreply.github.com>
2026-06-11 15:34:03 +0200

    feat(venue): add AuditoriumRepository and SeatRepository

---
+++ src/main/java/dev/eric_muganga/cinema/venue/repository/SeatRepository.java
@@ -0,0 +1,13 @@
+package dev.eric_muganga.cinema.venue.repository;
+
+import dev.eric_muganga.cinema.venue.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+    List<Seat> findByAuditorium_Id(Long auditoriumId);
+    List<Seat> findByAuditoriumIdAndSeatType(Long auditoriumId, String seatType);
+}
