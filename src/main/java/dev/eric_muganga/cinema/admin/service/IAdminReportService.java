69773cd3662b2540ab749f7de44a779e594f3f20
eric-muganga <105128548+eric-muganga@users.noreply.github.com>
2026-07-23 18:10:20 +0200

    refactor(admin): move IAdminReportService into admin service package

--- src/main/java/dev/eric_muganga/cinema/admin/service/IAdminReportService.java
+++ src/main/java/dev/eric_muganga/cinema/admin/service/IAdminReportService.java
@@ -1,11 +1,10 @@
-package dev.eric_muganga.cinema.reservation.service;
+package dev.eric_muganga.cinema.admin.service;
 
 import dev.eric_muganga.cinema.reservation.dto.PagedShowtimeReportResponse;
 import dev.eric_muganga.cinema.reservation.dto.ShowtimeReport;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface IAdminReportService {
     ShowtimeReport getShowtimeReport(Long showtimeId);
